7ca2f1c57955dd990616760a241d4ddc2dd9fcba
okayishcoder <yashchauhan071292@gmail.com>
2024-07-19 16:57:39 +0530

    Services section done

---
+++ src/components/services/serviceicon.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const ServiceIcon = (props) => {
+  const [iconDisplay, setIconDisplay] = useState("none");
+  const [iconColor, setIconColor] = useState("#4c4a4a");
+
+  const onServiceIconHoverIn = () => {
+    setIconDisplay("block");
+    setIconColor("#000000");
+  };
+
+  const onServiceIconHoverOut = () => {
+    setIconDisplay("none");
+    setIconColor("#4c4a4a");
+  };
+
+  return (
+    <div>
+      <div>
+        <p className="service-icon-text" style={{ display: iconDisplay }}>
+          {props.title}
+        </p>
+      </div>
+      <svg
+        onMouseOver={onServiceIconHoverIn}
+        onMouseOut={onServiceIconHoverOut}
+        xmlns="http://www.w3.org/2000/svg"
+        id="Layer_1"
+        data-name="Layer 1"
+        viewBox="0 0 24 24"
+        fill={iconColor}
+        width="30"
+        height="30"
+      >
+        <path d={props.path} />
+      </svg>
+    </div>
+  );
+};
+
+export default ServiceIcon;
--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,21 +1,8 @@
 import { Image, Row, Col, Container } from "react-bootstrap";
 
 import servicesBackgroundImg from "../../assets/images/ServicesBackground.png";
-import BulbIcon from "./icons/bulbIcon";
-import KeypadIcon from "./icons/keypadIcon";
-import FanIcon from "./icons/fanIcon";
-import AcIcon from "./icons/acIcon";
-import TvIcon from "./icons/tvIcon";
-import StpIcon from "./icons/stpIcon";
-import ProjectorIcon from "./icons/projectorIcon";
-import CurtainIcon from "./icons/curtainIcon";
-import VisitorIcon from "./icons/visitorIcon";
-import GateIcon from "./icons/gateIcon";
-import CctvIcon from "./icons/cctvIcon";
-import MusicIcon from "./icons/musicIcon";
-import SecurityIcon from "./icons/securityIcon";
-import HometheaterIcon from "./icons/hometheaterIcon";
-import NetworkingIcon from "./icons/networkingIcon";
+import ServiceIcon from "./serviceicon";
+import SERVICES_ICON_DATA from "./serviceicondata";
 
 export default function Services() {
   return (
@@ -29,56 +16,16 @@ export default function Services() {
           in the world.
         </p>
         <Container>
-          <Row className="services-icon-row">
-            <Col>
-              <BulbIcon color="grey" />
-            </Col>
-            <Col>
-              <KeypadIcon color="grey" />
-            </Col>
-            <Col>
-              <FanIcon color="grey" />
-            </Col>
-            <Col>
-              <AcIcon color="grey" />
-            </Col>
-            <Col>
-              <TvIcon color="grey" />
-            </Col>
-          </Row>
-          <Row className="services-icon-row">
-            <Col>
-              <StpIcon color="grey" />
-            </Col>
-            <Col>
-              <ProjectorIcon color="grey" />
-            </Col>
-            <Col>
-              <CurtainIcon color="grey" />
-            </Col>
-            <Col>
-              <VisitorIcon color="grey" />
-            </Col>
-            <Col>
-              <GateIcon color="grey" />
-            </Col>
-          </Row>
-          <Row className="services-icon-row">
-            <Col>
-              <CctvIcon color="grey" />
-            </Col>
-            <Col>
-              <MusicIcon color="grey" />
-            </Col>
-            <Col>
-              <SecurityIcon color="grey" />
-            </Col>
-            <Col>
-              <HometheaterIcon color="grey" />
-            </Col>
-            <Col>
-              <NetworkingIcon color="grey" />
-            </Col>
+          <Row className="services-icon-row" sm={5}>
+            {SERVICES_ICON_DATA.map((colData, i) => (
+              <Col key={colData.title} className="service-icon-col">
+                <ServiceIcon
+                  title={colData.title}
+                  color={colData.color}
+                  path={colData.path}
+                />
+              </Col>
+            ))}
           </Row>
         </Container>
       </div>
